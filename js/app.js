8df9628db0a020e208f83a3057798d2861259cf6
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-12-07 17:34:20 -0500

    Updates

---
+++ js/app.js
@@ -0,0 +1,38 @@
+var ViewModel = function() {
+  this.currentCat = ko.observable( new Cat () );
+
+  this.incrementCounter = function() {
+
+    this.currentCat.clickCount(this.currentCat.clickCount() + 1);
+  };
+}
+
+var Cat = function() {
+
+  ko.applyBindings(new ViewModel());
+
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribution = ko.observable('https://www.flickr.com/photos');
+  this.nicknames = ko.observableArray(['Nice Cat', 'Good Cat', 'Tippy', 'Koko', 'Kiki']);
+
+  this.title = ko.computed(function() {
+    var title;
+    var clicks = this.clickCount();
+    if (clicks < 10) {
+      title='Newborn';
+    } else if (clicks < 50) {
+      title='Infant';
+    } else if (clicks < 100){
+      title='Child';
+    } else if (clicks < 200){
+    title='Teen';
+    } else if (clicks < 500){
+    title='Adult';
+    } else {
+    title='Ninja';
+    }
+    return title;
+  },this);
+}
