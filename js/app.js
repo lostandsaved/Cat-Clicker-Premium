9af5f053ecaa5415201963f3ed27632437e9e28d
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-12-07 17:55:13 -0500

    Fixes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 var Cat = function() {
 
-  ko.applyBindings(new ViewModel());
-
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
@@ -15,22 +13,25 @@ var Cat = function() {
       title='Newborn';
     } else if (clicks < 50) {
       title='Infant';
-    } else if (clicks < 100){
+    } else if (clicks < 100) {
       title='Child';
-    } else if (clicks < 200){
+    } else if (clicks < 200) {
     title='Teen';
-    } else if (clicks < 500){
+    } else if (clicks < 500) {
     title='Adult';
     } else {
     title='Ninja';
     }
     return title;
   },this);
+
 }
 
 var ViewModel = function() {
-  this.currentCat = ko.observable( new Cat () );
+  this.currentCat = ko.observable( new Cat() );
   this.incrementCounter = function() {
     this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   };
 };
+
+ko.applyBindings(new ViewModel());
