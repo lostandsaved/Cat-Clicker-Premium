f76dd78ec47fc37ee46d6e964a1b041198d39699
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-12-07 19:40:19 -0500

    complete cat clicker with knockout

--- js/app.js
+++ js/app.js
@@ -1,10 +1,49 @@
-var Cat = function() {
+var initialCats = [
+  {
+      clickCount : 0,
+      name : 'Tabby',
+      imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+      imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+      nicknames: ['Tigger', 'T-Bone', 'Mr. T', 'Tabitha Tab Tab Catty Cat']
 
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('https://www.flickr.com/photos');
-  this.nicknames = ko.observableArray(['Nice Cat', 'Good Cat', 'Tippy', 'Koko', 'Kiki']);
+  },
+  {
+      clickCount : 0,
+      name : 'Tiger',
+      imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+      imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+      nicknames: ['Tigger']
+  },
+  {
+      clickCount : 0,
+      name : 'Scaredy',
+      imgSrc : 'img/22252709_010df3379e_z.jpg',
+      imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+      nicknames: ['Casper']
+  },
+  {
+      clickCount : 0,
+      name : 'Shadow',
+      imgSrc : 'img/1413379559_412a540d29_z.jpg',
+      imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+      nicknames: ['Shooby']
+  },
+  {
+      clickCount : 0,
+      name : 'Sleepy',
+      imgSrc : 'img/9648464288_2516b35537_z.jpg',
+      imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+      nicknames: ['Zzzz']
+  }
+]
+
+var Cat = function(data) {
+
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.title = ko.computed(function() {
     var title;
@@ -27,10 +66,25 @@ var Cat = function() {
 
 }
 
+
+
 var ViewModel = function() {
-  this.currentCat = ko.observable( new Cat() );
+  var self = this;
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem) {
+    self.catList.push (new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable( this.catList() [0] );
+
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    //this.clickCount(this.clickCount() + 1);  --this is commented out when using self
+    self.currentCat().clickCount(self.currentCat().clickCount() +1);
+
+  };
+  this.setCat = function(clickedCat) {
+    self.currentCat(clickedCat)
   };
 };
 
